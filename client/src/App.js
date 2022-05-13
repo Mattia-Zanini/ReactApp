55bfbc7bb42f360fd498ae881d8e2396d874a317
Mattia-Zanini <mattzanini2@gmail.com>
2022-05-13 09:19:40 +0200

    piccoli cambiamenti

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import './App.css';
+import './style/scrollBar.css';
+
 import Navbar from './components/navbar';
 import ItemList from './components/itemList';
+//import ParallaxSection from './components/parallax';
 
 class App extends Component {
   render() {
     return (
       <>
         <Navbar />
-        <div className="container-fluid">
+        <div className="App container-fluid">
           <ItemList />
         </div>
       </>
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -6,7 +6,7 @@ import logo from '../images/logo_CANDY_MARKET.png';
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-dark bg-dark">
+      <nav className="navbar navbar-dark sticky-top">
         <div className="container-fluid">
           <span className="navbar-brand mb-0 h1">
             <img src={logo} alt="..." className="nav-logo" />
