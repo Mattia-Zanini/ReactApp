cf8b32a1cf5d847ca240f69fc588d5ff1b405ef7
Mattia-Zanini <mattzanini2@gmail.com>
2022-05-13 15:40:20 +0200

    navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import '../style/navbar.css';
 
 import logo from '../images/logo_CANDY_MARKET.png';
+import user from '../images/uicons/user.svg';
+import shopBag from '../images/uicons/shopping-bag.svg';
 
 class Navbar extends Component {
   render() {
@@ -11,6 +13,20 @@ class Navbar extends Component {
           <span className="navbar-brand mb-0 h1">
             <img src={logo} alt="..." className="nav-logo" />
           </span>
+          <div class="navbar-nav">
+            <div className="row">
+              <div className="col">
+                <a class="nav-link" href="" style={{ color: 'black' }}>
+                  <img src={shopBag} alt="..." className="nav-shop" />
+                </a>
+              </div>
+              <div className="col">
+                <a class="nav-link" href="" style={{ color: 'black' }}>
+                  <img src={user} alt="..." className="nav-usr" />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </nav>
     );
--- client/src/components/parallax.js
+++ client/src/components/parallax.js
@@ -1,31 +1,39 @@
 import React, { Component } from 'react'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
+import '../style/parallax.css';
+
 class ParallaxSection extends Component {
     render() {
         return (
-            <Parallax pages={2} style={{ top: '0', left: '0' }}>
-                <ParallaxLayer
-                    offset={0}
-                    speed={2.5}
-                    style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <p>Candy Market</p>
-                </ParallaxLayer>
+            <>
+                <Parallax pages={2} style={{ top: '0', left: '0' }}>
+                    <ParallaxLayer
+                        offset={0}
+                        speed={3}
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }}>
+                        <p className="parallax-P">Candy Market</p>
+                    </ParallaxLayer>
 
-                <ParallaxLayer offset={1} speed={2} style={{ backgroundColor: '#ff6d6d' }} />
+                    <ParallaxLayer offset={1} speed={3} style={{ backgroundColor: '#c79ce2' }} />
 
-                <ParallaxLayer
-                    offset={1}
-                    speed={0.5}
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        color: 'white',
-                    }}>
-                    <p>Scopri i nostri prodotti</p>
-                </ParallaxLayer>
-            </Parallax>
+                    <ParallaxLayer
+                        offset={1}
+                        speed={1.5}
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            color: 'white',
+                        }}>
+                        <p className="parallax-P">Scopri i nostri prodotti</p>
+                    </ParallaxLayer>
+                </Parallax>
+            </>
         );
     }
 }
